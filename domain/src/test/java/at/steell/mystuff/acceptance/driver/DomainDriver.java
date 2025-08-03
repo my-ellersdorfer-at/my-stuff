c996de835b377ef34cc069f3ff51f2660f6abe92
Stefan Ellersdorfer <stefan.ellersdorfer@smarter-software.com>
2025-08-03 06:51:14 +0200

    Implement DomainDriver: Integrate create asset and find asset into domain acceptance test

--- domain/src/test/java/at/steell/mystuff/acceptance/driver/DomainDriver.java
+++ domain/src/test/java/at/steell/mystuff/acceptance/driver/DomainDriver.java
@@ -1,8 +1,16 @@
 package at.steell.mystuff.acceptance.driver;
 
+import at.steell.mystuff.domain.entity.Asset;
+import at.steell.mystuff.domain.interactor.AssetInteractor;
+
 import java.util.function.Supplier;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class DomainDriver implements MyStuffAcceptanceDriver {
+    private final AssetInteractor assetInteractor = new AssetInteractor();
+
     @Override
     public void authenticateAsUser(final String username) {
 
@@ -20,12 +28,14 @@ public class DomainDriver implements MyStuffAcceptanceDriver {
 
     @Override
     public String createAsset(final AssetOptions assetOptions) {
-        return "";
+        return assetInteractor.createAsset();
     }
 
     @Override
     public void assertThatAssetExists(final String assetId) {
-
+        Asset asset = assetInteractor.find(assetId);
+        assertNotNull(asset);
+        assertEquals(assetId, asset.getId());
     }
 
     @Override
