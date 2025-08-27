2c14803a3bf202fca802060298d57e29e84eafed
Stefan Ellersdorfer <22933992+steell0815@users.noreply.github.com>
2025-08-27 15:59:38 +0200

    Elaborate domain driver for acceptance testing against the domain

--- domain/src/test/java/at/steell/mystuff/acceptance/driver/DomainDriver.java
+++ domain/src/test/java/at/steell/mystuff/acceptance/driver/DomainDriver.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DomainDriver implements MyStuffAcceptanceDriver {
