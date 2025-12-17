f69d24722c8ac4871713a1401be354a5d585ea81
Stefan Ellersdorfer <22933992+steell0815@users.noreply.github.com>
2025-12-17 12:45:22 +0100

    Fix OauthSecurityChainConfiguration

--- application/src/main/java/at/steell/mystuff/application/configuration/OauthSecurityChainConfiguration.java
+++ application/src/main/java/at/steell/mystuff/application/configuration/OauthSecurityChainConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
