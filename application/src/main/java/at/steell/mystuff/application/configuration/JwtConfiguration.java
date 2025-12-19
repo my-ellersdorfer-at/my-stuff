69a3665610dff63923dea95d721451f2b9dfd053
Stefan Ellersdorfer <22933992+steell0815@users.noreply.github.com>
2025-12-19 07:03:26 +0100

    Unused import - remove Configuration annotation from JwtConfiguration

--- application/src/main/java/at/steell/mystuff/application/configuration/JwtConfiguration.java
+++ application/src/main/java/at/steell/mystuff/application/configuration/JwtConfiguration.java
@@ -4,7 +4,6 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
