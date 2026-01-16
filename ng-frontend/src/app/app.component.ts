91fe8ac8f9bd46d1dea32080e52f4eb69ebfc902
Stefan Ellersdorfer <22933992+steell0815@users.noreply.github.com>
2026-01-16 16:43:03 +0100

    Bump angular 21.0.7 -> 21.1.0

--- ng-frontend/src/app/app.component.ts
+++ ng-frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   readonly navItems = [
